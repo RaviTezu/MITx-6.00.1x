0b2de66325a274458362ed636f644b957bf1965e
ravitezu <ravi-teja@live.com>
2014-08-08 21:26:22 +0530

    Done

--- week-3/hangman_part_2:_the_game.py
+++ week-3/hangman_part_2:_the_game.py
@@ -74,3 +74,52 @@ def hangman(secretWord):
                  print "------------"
     return ('Sorry, you ran out of guesses. The word was ') + secretWord
 
+
+def hangman(secretWord):
+    '''
+    secretWord: string, the secret word to guess.
+
+    Starts up an interactive game of Hangman.
+
+    * At the start of the game, let the user know how many 
+      letters the secretWord contains.
+
+    * Ask the user to supply one guess (i.e. letter) per round.
+
+    * The user should receive feedback immediately after each guess 
+      about whether their guess appears in the computers word.
+
+    * After each round, you should also display to the user the 
+      partially guessed word so far, as well as letters that the 
+      user has not yet guessed.
+
+    Follows the other limitations detailed in the problem write-up.
+    '''
+    # FILL IN YOUR CODE HERE...
+    wl = len(secretWord)
+    lettersGuessed = []
+    mistakesMade = 0
+    print 'Welcome to the game, Hangman!'
+    print 'I am thinking of a word that is ' + str(wl) + ' letters long.'
+    print '------------'
+    while mistakesMade < 8:
+        if isWordGuessed(secretWord, lettersGuessed):
+            return "Congratulations, you won!"
+        else:
+            print "You have " + str(8-mistakesMade) + " guesses left."
+            print "Available letters: " + getAvailableLetters(lettersGuessed)
+            guess = raw_input("Please guess a letter: ").lower()
+            if not guess in lettersGuessed:
+                lettersGuessed.append(guess)
+                if guess in secretWord:
+                    print "Good guess: " + getGuessedWord(secretWord, lettersGuessed)
+                    print "------------"
+                else:
+                    mistakesMade = mistakesMade + 1
+                    print "Oops! That letter is not in my word: " + getGuessedWord(secretWord, lettersGuessed)
+                    print "------------"
+            else:
+                 print "Oops! You've already guessed that letter: " + getGuessedWord(secretWord, lettersGuessed)
+                 print "------------"
+    return "Sorry, you ran out of guesses. The word was " + secretWord
+
